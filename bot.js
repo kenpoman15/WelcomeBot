2223d121c1503c74ad113fc7c18c439aad7b4017
kenpoman15 <taft.aj15@yahoo.com>
2019-10-22 22:28:52 -0400

    Add files via upload

--- bot.js
+++ bot.js
@@ -10,15 +10,34 @@ bot.login(auth.token);
 
 bot.on('message', msg => {
 	
-	//Update user to guest
-    if (msg.content == '!agree') {
-		//console.log(msg.client.user + ' has agreed to the rules');
+	//Checking for command
+	var msgtext = msg.toString();
+	if(msgtext.substring(0,1) == '!')
+	{
+		var msgArray = msgtext.split(" ");
+		var cmd = msgArray[0].substring(1);
 		
-		//Find and assign the guest role
-		var role = msg.guild.roles.find(role => role.name === "Guest");
-		msg.member.addRole(role);
+		switch(cmd.toLowerCase()) {
+			//Simple ping pong
+			case "ping":
+				msg.reply("Pong!");
+				console.log("Pong!");
+				break;
 		
-		//Delete the message
-		msg.delete();
-    }
+			//Agreeing to T&Cs's
+			case "agree":
+				//Who wrote the message
+				console.log(msg.author.tag + ' has agreed to the rules');
+		
+				//Find and assign the guest role
+				var role = msg.guild.roles.find(role => role.name === "Guest");
+				msg.member.addRole(role);
+		
+				//Delete the message
+				msg.delete();
+				break;
+		}
+		
+		console.log(cmd);
+	}
 });
